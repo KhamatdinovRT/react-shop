f3a09ddd312ef590f08a256d6e590a6f644c53e1
Ruslan <khamatdinovrt@bashedu.ru>
2017-06-13 20:57:08 +0500

    routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import Header from './Header';
 import {ShopList} from './ShopList';
+import {ShopHome} from './ShopHome';
 
-const App = () => (
-  <div>
-    <Header/>
-    <ShopList></ShopList>
-  </div>
-)
+class App extends Component {
+  render() {
+    return(
+    <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={ShopHome}/>
+          <Route path="/list" component={ShopList}/>
+        </Switch>
+    </div>
+    );
+  }
+}
 export default App;

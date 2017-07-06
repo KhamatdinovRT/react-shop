0f3f5ec09946a668e79e4e93d6c9baf188824576
Ruslan <khamatdinovrt@bashedu.ru>
2017-07-06 20:22:30 +0500

    shop home changes

--- src/components/ShopHome.js
+++ src/components/ShopHome.js
@@ -1,48 +1,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import styles from'./ShopHome.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+
+const PUBLIC_URL = process.env.PUBLIC_URL;
 
 class ShopHome extends Component {
     constructor() {
         super();
         this.state = {
-            categories: [
-                {
-                    name: 'mens_outerwear',
-                    title: 'Men\'s Outerwear',
-                    image: 'images/mens_outerwear.jpg',
-                    placeholder: 'data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAXAABAQEAAAAAAAAAAAAAAAAAAAIEAQEAAAAAAAAAAAAAAAAAAAACEAAAAwYHAQAAAAAAAAAAAAAAERMBAhIyYhQhkaEDIwUVNREBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8A3dkr5e8tfpwuneJITOzIcmQpit037Bw4mnCVNOpAAQv/2Q=='
-                },
-                {
-                    name: 'ladies_outerwear',
-                    title: 'Ladies Outerwear',
-                    image: 'images/ladies_outerwear.jpg',
-                    placeholder: 'data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAWQABAQAAAAAAAAAAAAAAAAAAAAEBAQEAAAAAAAAAAAAAAAAAAAIDEAABAwMFAQAAAAAAAAAAAAARAAEygRIDIlITMwUVEQEBAAAAAAAAAAAAAAAAAAAAQf/aAAwDAQACEQMRAD8Avqn5meQ0kwk1UyclmLtNj7L4PQoioFf/2Q=='
-                },
-                {
-                    name: 'mens_tshirts',
-                    title: 'Men\'s T-Shirts',
-                    image: 'images/mens_tshirts.jpg',
-                    placeholder: 'data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAWwABAQEAAAAAAAAAAAAAAAAAAAMEAQEAAAAAAAAAAAAAAAAAAAAAEAABAwEJAAAAAAAAAAAAAAARAAESEyFhodEygjMUBREAAwAAAAAAAAAAAAAAAAAAAEFC/9oADAMBAAIRAxEAPwDb7kupZU1MTGnvOCgxpvzEXTyRElCmf//Z'
-                },
-                {
-                    name: 'ladies_tshirts',
-                    title: 'Ladies T-Shirts',
-                    image: 'images/ladies_tshirts.jpg',
-                    placeholder: 'data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAXwABAQEAAAAAAAAAAAAAAAAAAAMFAQEBAAAAAAAAAAAAAAAAAAABAhAAAQIDCQAAAAAAAAAAAAAAEQABITETYZECEjJCAzMVEQACAwAAAAAAAAAAAAAAAAAAATFBgf/aAAwDAQACEQMRAD8AzeADAZiFc5J7BC9Scek3VrtooilSNaf/2Q=='
-                }
-            ]
+            categories: []
         }
-    }      
+        this.loadCategories()
+    }
+
+    loadCategories() {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'data/categories.json');
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                this.setState ({categories: JSON.parse(xhr.responseText)})
+            }
+        };
+        xhr.send();        
+    }
+
     render() {
         console.log(this.state.categories);
         return (
-            <div>
+            <div className={styles.container}>
                 {this.state.categories.map ((item)=> (
                      <div className={styles.item}>
                         <Link to={`/list/${item.name}`}>
-                            <img src={require("../"+ item.image)} alt=""/>
+                            <img src={PUBLIC_URL + item.image} alt=""/>
                         </Link>
                         <h2>{item.title}</h2>
                      </div>    
